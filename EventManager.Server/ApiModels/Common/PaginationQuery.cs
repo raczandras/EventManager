7175f586c231de200cda6aae491e9f2b240814ea
András Rácz <racz.andras@msk.hu>
2025-09-23 18:17:43 +0200

    Common Dto-s for pagination

--- EventManager.Server/ApiModels/Common/PaginationQuery.cs
+++ EventManager.Server/ApiModels/Common/PaginationQuery.cs
@@ -1,6 +1,27 @@
-﻿namespace EventManager.Server.ApiModels.Common
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.Server.ApiModels.Common
 {
-    public class PaginationQuery
+    public class PaginationQuery : IValidatableObject
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Page must be greater than 0.")]
+        public int? Page { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "PageSize must be greater than 0.")]
+        public int? PageSize { get; set; }
+
+        public string? SortBy { get; set; }
+        public bool Descending { get; set; } = false;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if ((Page.HasValue && !PageSize.HasValue) || (!Page.HasValue && PageSize.HasValue))
+            {
+                yield return new ValidationResult(
+                    "Both Page and PageSize must be provided together.",
+                    [nameof(Page), nameof(PageSize)]
+                );
+            }
+        }
     }
 }
