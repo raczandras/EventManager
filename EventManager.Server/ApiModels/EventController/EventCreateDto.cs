b2b438866d5f69d06593942a33ff538c210b6030
András Rácz <racz.andras@msk.hu>
2025-09-23 18:14:51 +0200

    Event Dto object

--- EventManager.Server/ApiModels/EventController/EventCreateDto.cs
+++ EventManager.Server/ApiModels/EventController/EventCreateDto.cs
@@ -1,6 +1,19 @@
-﻿namespace EventManager.Server.ApiModels.EventController
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.Server.ApiModels.EventController
 {
     public class EventCreateDto
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required.")]
+        public required string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Location is required.")]
+        [StringLength(100)]
+        public required string Location { get; set; }
+
+        public string? Country { get; set; }
+
+        [Range(1, uint.MaxValue, ErrorMessage = "Capacity must be a positive number.")]
+        public uint? Capacity { get; set; }
     }
 }
