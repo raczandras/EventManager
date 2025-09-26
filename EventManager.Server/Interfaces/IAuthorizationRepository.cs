77d529fceadeb5b67eccb4566762a0d31828b400
András Rácz <racz.andras@msk.hu>
2025-09-26 11:21:09 +0200

    auth repository

---
+++ EventManager.Server/Interfaces/IAuthorizationRepository.cs
@@ -0,0 +1,11 @@
+﻿using EventManager.Server.Data.Entities;
+
+namespace EventManager.Server.Interfaces
+{
+    public interface IAuthorizationRepository
+    {
+        Task SaveRefreshTokenAsync(RefreshToken token);
+        Task<RefreshToken?> GetRefreshTokenAsync(string token);
+        Task InvalidateRefreshTokenAsync(RefreshToken token);
+    }
+}
---
+++ EventManager.Server/Repositories/AuthorizationRepository.cs
@@ -0,0 +1,35 @@
+﻿using EventManager.Server.Data.Context;
+using EventManager.Server.Data.Entities;
+using EventManager.Server.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManager.Server.Repositories
+{
+    public class AuthorizationRepository : IAuthorizationRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AuthorizationRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task SaveRefreshTokenAsync(RefreshToken token)
+        {
+            _dbContext.RefreshTokens.Add(token);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<RefreshToken?> GetRefreshTokenAsync(string token)
+        {
+            return await _dbContext.RefreshTokens.Include(rt => rt.User).AsNoTracking().FirstOrDefaultAsync(rt => rt.Token == token && !rt.IsRevoked);
+        }
+
+        public async Task InvalidateRefreshTokenAsync(RefreshToken token)
+        {
+            token.IsRevoked = true;
+            _dbContext.RefreshTokens.Update(token);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
