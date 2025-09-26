ca156ff78e9acc047388bc1f63791260ea3ebf24
András Rácz <racz.andras@msk.hu>
2025-09-26 11:20:08 +0200

    custom password validation

---
+++ EventManager.Server/ValidationAttributes/PasswordAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.Server.ValidationAttributes
+{
+    public class PasswordAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not string password)
+                return new ValidationResult("Password is required.");
+
+            if (!password.Any(char.IsLower))
+                return new ValidationResult("Password must contain at least one lowercase letter.");
+            if (!password.Any(char.IsUpper))
+                return new ValidationResult("Password must contain at least one uppercase letter.");
+            if (!password.Any(char.IsDigit))
+                return new ValidationResult("Password must contain at least one number.");
+
+            return ValidationResult.Success;
+        }
+    }
+}
