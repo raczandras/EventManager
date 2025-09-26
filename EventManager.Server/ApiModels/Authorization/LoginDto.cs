e59d50e1e61ea0ec140aea6640765ac32d1df66c
András Rácz <racz.andras@msk.hu>
2025-09-26 11:20:20 +0200

    Authorization DTOs

---
+++ EventManager.Server/ApiModels/Authorization/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EventManager.Server.ValidationAttributes;
+
+namespace EventManager.Server.ApiModels.Authorization
+{
+    public class LoginDto
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = $"{nameof(Email)} is required.")]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Password]
+        public required string Password { get; set; }
+    }
+}
---
+++ EventManager.Server/ApiModels/Authorization/RefreshRequestsDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.Server.ApiModels.Authorization
+{
+    public class RefreshTokenDto
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = $"{nameof(RefreshToken)} is required.")]
+        public required string RefreshToken { get; set; }
+    }
+}
