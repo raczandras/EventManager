74e4b7c894f31b9ea2f8c6db1e253e45789c7eeb
András Rácz <racz.andras@msk.hu>
2025-09-26 12:26:32 +0200

    Display proper email in the header

--- eventmanager.client/src/components/AppHeader.tsx
+++ eventmanager.client/src/components/AppHeader.tsx
@@ -1,7 +1,11 @@
 import { AppBar, Toolbar, Typography, Avatar, Box } from "@mui/material";
 import mxcLogo from "../assets/mxc_logo.png";
+import { useAuth } from "../context/AuthContext";
+import { NavLink } from "react-router-dom";
 
 export default function AppHeader() {
+    const { email, logout } = useAuth();
+
     return (
         <AppBar position="static" color="default">
             <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
@@ -10,8 +14,12 @@ export default function AppHeader() {
                 </Box>
 
                 <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-                    <Typography variant="body1">user@example.com</Typography>
-                    <Avatar alt="Profile" src="/logo192.png" sx={{ width: 40, height: 40 }} />
+                    <Box sx={{ display: "flex", flexDirection: "column" }}>
+                        <Typography variant="body1">{email}</Typography>
+                        <NavLink to="/login" onClick={logout} style={{ textDecoration: "none", color: "inherit", textAlign: "right" }}><Typography variant="body1">Logout</Typography></NavLink>
+                    </Box>
+                                         {/*This won't exist but oh well*/}
+                    <Avatar alt="Profile" src="/logo192.png" sx={{ width: 40, height: 40 }} />                
                 </Box>
             </Toolbar>
         </AppBar>
